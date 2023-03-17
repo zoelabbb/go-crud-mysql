bc6fd3b18054296d49c0aa4703169bdfd5d24f88
zoelabbb <zoelabbb@gmail.com>
2023-03-17 23:51:15 +0700

    Update

---
+++ controllers/pasiencontroller/pasiencontroller.go
@@ -0,0 +1,16 @@
+package pasiencontroller
+
+import "net/http"
+
+func Index(response http.ResponseWriter, request *http.Request) {
+
+}
+func Edit(response http.ResponseWriter, request *http.Request) {
+
+}
+func Add(response http.ResponseWriter, request *http.Request) {
+
+}
+func Delete(response http.ResponseWriter, request *http.Request) {
+
+}
---
+++ entities/pasien.go
@@ -0,0 +1,12 @@
+package entities
+
+type Pasien struct {
+	Id           int64
+	NamaLengkap  string
+	NIK          string
+	JenisKelamin string
+	TempatLahir  string
+	TanggalLahir string
+	Alamat       string
+	NoHp         string
+}
