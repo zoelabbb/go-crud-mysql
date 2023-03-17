23c2e85499e16453a3b862468b5dc96cba44531e
zoelabbb <zoelabbb@gmail.com>
2023-03-18 02:12:10 +0700

    New Update Insert into DB

--- controllers/pasiencontroller/pasiencontroller.go
+++ controllers/pasiencontroller/pasiencontroller.go
@@ -1,17 +1,27 @@
 package pasiencontroller
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func Index(response http.ResponseWriter, request *http.Request) {
-	fmt.Println("Bagus,Server berhasil berjalan :)")
-}
-func Edit(response http.ResponseWriter, request *http.Request) {
+
+	temp, err := template.ParseFiles("views/pasien/index.html")
+	if err != nil {
+		panic(err)
+	}
+	temp.Execute(response, nil)
 
 }
 func Add(response http.ResponseWriter, request *http.Request) {
+	temp, err := template.ParseFiles("views/pasien/add.html")
+	if err != nil {
+		panic(err)
+	}
+	temp.Execute(response, nil)
+}
+func Edit(response http.ResponseWriter, request *http.Request) {
 
 }
 func Delete(response http.ResponseWriter, request *http.Request) {
